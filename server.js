5d0af90dbc3d07a3650cc4e239401dbd74427505
yskm <y.morita0216@gmail.com>
2015-03-03 01:11:21 +0900

    Use session

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-var express = require('express'),
-    app = express(),
-    http = require('http').Server(app);
+var express = require('express');
+var app = express();
+var session = require('express-session');
 
-http.listen(3000, function() {
+app.listen(3000, function() {
     console.log('listening on *:3000');
 });
 
+app.use(session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.get('/', function(req, res) {
-    res.send('initialized');
+    if (req.session.views) {
+        req.session.views++;
+    }
+    else {
+        req.session.views = 1;
+    }
+    res.write('<p>views: ' + req.session.views + '</p>');
+    res.end();
 });
