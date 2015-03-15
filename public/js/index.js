82a51fd96e73408c920b5f41cda3e021c68145f5
yskm <y.morita0216@gmail.com>
2015-03-16 01:17:53 +0900

    tab length 4 to 2

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,15 @@
 (function() {
-	var socket = require('socket.io-client')();
+  var socket = require('socket.io-client')();
 
-	socket.on('new_post', function(data) {
-		for (var i = 0; i < data.posts.length; i++) {
-			var post = data.posts[i];
-			if (post.type === 'photo') {
-				$('<article>')
-					.attr('class', 'photo')
-					.append($('<img>').attr('src', post.photos[0].original_size.url))
-					.prependTo($('#posts'));
-			}
-		}
-	});
+  socket.on('new_post', function(data) {
+    for (var i = 0; i < data.posts.length; i++) {
+      var post = data.posts[i];
+      if (post.type === 'photo') {
+      $('<article>')
+        .attr('class', 'photo')
+        .append($('<img>').attr('src', post.photos[0].original_size.url))
+        .prependTo($('#posts'));
+      }
+    }
+  });
 })();
--- server.js
+++ server.js
@@ -13,74 +13,74 @@ var tumblr_consumer_secret = process.env.TUMBLR_CONSUMER_SECRET;
 
 var OAuth = require('oauth').OAuth;
 var oa = new OAuth(
-        'http://www.tumblr.com/oauth/request_token',
-        'http://www.tumblr.com/oauth/access_token',
-        tumblr_consumer_key,
-        tumblr_consumer_secret,
-        '1.0',
-        'http://localhost:3000/callback',
-        'HMAC-SHA1'
-    );
+  'http://www.tumblr.com/oauth/request_token',
+  'http://www.tumblr.com/oauth/access_token',
+  tumblr_consumer_key,
+  tumblr_consumer_secret,
+  '1.0',
+  'http://localhost:3000/callback',
+  'HMAC-SHA1'
+);
 
 io.use(function(socket, next) {
-    var hashedCookies = require('cookie').parse(socket.request.headers.cookie);
-    var cookies = require('cookie-parser/lib/parse').signedCookies(hashedCookies, session_secret);
-    var sid = cookies['connect.sid'];
+  var hashedCookies = require('cookie').parse(socket.request.headers.cookie);
+  var cookies = require('cookie-parser/lib/parse').signedCookies(hashedCookies, session_secret);
+  var sid = cookies['connect.sid'];
 
-    sessionStore.get(sid, function(err, session) {
-        if (err) {
-            console.log(err);
-            return;
-        }
+  sessionStore.get(sid, function(err, session) {
+    if (err) {
+      console.log(err);
+      return;
+    }
 
-        socket.session = session;
-        next();
-    });
+    socket.session = session;
+    next();
+  });
 });
 
 io.on('connection', function(socket) {
-    console.log('socket.io connected: ' + socket.id);
+  console.log('socket.io connected: ' + socket.id);
 
-    if (socket.session === undefined || socket.session.oauth_status !== 'authenticated') {
-        return;
-    }
-    var client = tumblr.createClient({
-        consumer_key: tumblr_consumer_key,
-        consumer_secret: tumblr_consumer_secret,
-        token: socket.session.oauth_access_token,
-        token_secret: socket.session.oauth_access_token_secret
-    });
-    var latest_post_id = socket.session.latest_post_id;
+  if (socket.session === undefined || socket.session.oauth_status !== 'authenticated') {
+    return;
+  }
+  var client = tumblr.createClient({
+    consumer_key: tumblr_consumer_key,
+    consumer_secret: tumblr_consumer_secret,
+    token: socket.session.oauth_access_token,
+    token_secret: socket.session.oauth_access_token_secret
+  });
+  var latest_post_id = socket.session.latest_post_id;
 
-    var interval =  setInterval(function() {
-        if (latest_post_id) {
-            client.dashboard({
-                since_id: latest_post_id
-            }, function(err, data) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-
-                if (data.posts !== undefined && data.posts.length > 0) {
-                    socket.emit('new_post', data);
-                    latest_post_id = data.posts[0].id;
-                }
-            });
+  var interval =  setInterval(function() {
+    if (latest_post_id) {
+      client.dashboard({
+          since_id: latest_post_id
+      }, function(err, data) {
+        if (err) {
+          console.log(err);
+          return;
         }
-    }, 5000);
 
-    socket.on('disconnect', function() {
-        console.log('socket.io disconnected: ' + socket.id);
-        clearInterval(interval);
-    });
+        if (data.posts !== undefined && data.posts.length > 0) {
+          socket.emit('new_post', data);
+          latest_post_id = data.posts[0].id;
+        }
+      });
+    }
+  }, 5000);
+
+  socket.on('disconnect', function() {
+    console.log('socket.io disconnected: ' + socket.id);
+    clearInterval(interval);
+  });
 });
 
 app.use(session({
-    secret: session_secret,
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false
+  secret: session_secret,
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false
 }));
 
 app.use(express.static(__dirname + '/public'));
@@ -88,75 +88,75 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-    if (req.session.oauth_status !== 'authenticated') {
-        res.redirect('/auth');
-    }
-    else {
-        var client = tumblr.createClient({
-            consumer_key: tumblr_consumer_key,
-            consumer_secret: tumblr_consumer_secret,
-            token: req.session.oauth_access_token,
-            token_secret: req.session.oauth_access_token_secret
-        });
+  if (req.session.oauth_status !== 'authenticated') {
+    res.redirect('/auth');
+  }
+  else {
+    var client = tumblr.createClient({
+      consumer_key: tumblr_consumer_key,
+      consumer_secret: tumblr_consumer_secret,
+      token: req.session.oauth_access_token,
+      token_secret: req.session.oauth_access_token_secret
+    });
 
-        client.dashboard(function(err, data) {
-            if (err) {
-                console.log(err);
-                res.end();
-                return;
-            }
+    client.dashboard(function(err, data) {
+      if (err) {
+        console.log(err);
+        res.end();
+        return;
+      }
 
-            req.session.latest_post_id = data.posts[0].id;
-            res.render('index', {title: '', posts: data.posts});
-        });
-    }
+      req.session.latest_post_id = data.posts[0].id;
+      res.render('index', {title: '', posts: data.posts});
+    });
+  }
 });
 
 app.get('/auth', function(req, res) {
-    if (req.session.oauth_status === 'authenticated') {
-        res.redirect('/');
-    }
-    else {
-        oa.getOAuthRequestToken(function(err, oauth_token, oauth_token_secret, results) {
-            if (err) {
-                console.log(err);
-                res.end();
-                return;
-            }
+  if (req.session.oauth_status === 'authenticated') {
+    res.redirect('/');
+  }
+  else {
+    oa.getOAuthRequestToken(function(err, oauth_token, oauth_token_secret, results) {
+      if (err) {
+        console.log(err);
+        res.end();
+        return;
+      }
 
-            req.session.oauth_token = oauth_token;
-            req.session.oauth_token_secret = oauth_token_secret;
-            req.session.oauth_status = 'initialized';
-            res.redirect('http://www.tumblr.com/oauth/authorize?oauth_token='+oauth_token);
-        });
-    }
+      req.session.oauth_token = oauth_token;
+      req.session.oauth_token_secret = oauth_token_secret;
+      req.session.oauth_status = 'initialized';
+      res.redirect('http://www.tumblr.com/oauth/authorize?oauth_token='+oauth_token);
+    });
+  }
 });
 
 app.get('/callback', function(req, res) {
-    if (req.session.oauth_status === 'initialized') {
-        oa.getOAuthAccessToken(
-            req.session.oauth_token,
-            req.session.oauth_token_secret,
-            req.query.oauth_verifier,
-            function(err, oauth_access_token, oauth_access_token_secret, results) {
-                if (err) {
-                    console.log(err);
-                    res.end();
-                    return;
-                }
+  if (req.session.oauth_status === 'initialized') {
+    oa.getOAuthAccessToken(
+      req.session.oauth_token,
+      req.session.oauth_token_secret,
+      req.query.oauth_verifier,
+      function(err, oauth_access_token, oauth_access_token_secret, results) {
+        if (err) {
+          console.log(err);
+          res.end();
+          return;
+        }
 
-                req.session.oauth_access_token = oauth_access_token;
-                req.session.oauth_access_token_secret = oauth_access_token_secret;
-                req.session.oauth_status = 'authenticated';
-                res.redirect('/');
-            }
-        );
-    }
-    else {
+        req.session.oauth_access_token = oauth_access_token;
+        req.session.oauth_access_token_secret = oauth_access_token_secret;
+        req.session.oauth_status = 'authenticated';
         res.redirect('/');
-    }
+      }
+    );
+  }
+  else {
+    res.redirect('/');
+  }
 });
 
 server.listen(3000, function() {
-    console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
